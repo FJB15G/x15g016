7fd2f468ad42b92d6846a9c91e792644e11eea0f
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-02 14:50:53 +0900

    １０３追加

---
+++ src/jp/ac/chiba/x15g016/kd103/X103.java
@@ -0,0 +1,10 @@
+package jp.ac.chiba.x15g016.kd103;
+
+public class X103 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      System.out.print("1 幸田拓馬 2　幸田拓馬　3 幸田拓馬 4 幸田拓馬 5 幸田拓馬 6 幸田拓馬 7 幸田拓馬 8 幸田拓馬 9 幸田拓馬 10 幸田拓馬");
+	}
+
+}
