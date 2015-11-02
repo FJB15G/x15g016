652eda25932b8cdef1484cbf3fbf7e99bf6f6d1c
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-02 15:05:31 +0900

    １０６追加

---
+++ src/jp/ac/chiba/x15g016/kd106/X106.java
@@ -0,0 +1,17 @@
+package jp.ac.chiba.x15g016.kd106;
+
+public class X106 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+           System.out.println("  a@@@a a@@@a");
+           System.out.println(" @@@@@@@@@@@@@");
+           System.out.println("(@@@@@@@@@@@@@)");
+           System.out.println(" @@@@@@@@@@@@@");
+           System.out.println("  @@@@@@@@@@@");
+           System.out.println("   ~@@@@@@@~");
+           System.out.println("    ~@@@~");
+           System.out.println("     ~@~");
+	}
+
+}
