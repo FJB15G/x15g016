a7673508c7aaa9b8da11214696addf1fa2839231
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-02 14:59:25 +0900

    １０５追加

---
+++ src/jp/ac/chiba/x15g016/kd105/X105.java
@@ -0,0 +1,11 @@
+package jp.ac.chiba.x15g016.kd105;
+
+public class X105 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+           System.out.println("氏名：幸田拓馬");
+           System.out.println("趣味：読書");
+	}
+
+}
