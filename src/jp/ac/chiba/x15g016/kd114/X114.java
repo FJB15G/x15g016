7e39f87588113969ac711f5060de560be3cb339f
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 15:07:55 +0900

    114

---
+++ src/jp/ac/chiba/x15g016/kd114/X114.java
@@ -0,0 +1,15 @@
+package jp.ac.chiba.x15g016.kd114;
+
+public class X114 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+       
+        int i;
+        for(i=0;i<5;i=i+1)
+     
+        System.out.println("***");
+        
+	
+	}
+}
