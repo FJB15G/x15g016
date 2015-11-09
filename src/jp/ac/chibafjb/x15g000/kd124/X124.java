5a17d46562b9f05790ef411578e1c7cdc3dc4f98
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 15:08:26 +0900

    124

---
+++ src/jp/ac/chibafjb/x15g000/kd124/X124.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g000.kd124;
+
+public class X124 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+        for(i=0;i<10;i=i+1)
+        	System.out.print("☆");
+	}
+
+}
