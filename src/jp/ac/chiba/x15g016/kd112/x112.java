9af67ac29df9eb407a8bfe120a68b74ef87073cb
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 14:06:20 +0900

    112

---
+++ src/jp/ac/chiba/x15g016/kd112/x112.java
@@ -0,0 +1,13 @@
+package jp.ac.chiba.x15g016.kd112;
+
+public class x112 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+         int i;
+         for(i=0;i<100;i=i+1)
+        	 System.out.println("幸田拓馬");
+        	 
+	}
+
+}
