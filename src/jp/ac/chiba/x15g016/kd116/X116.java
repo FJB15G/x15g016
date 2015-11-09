08d5e965370b4992c6b5a4afe87d1293077624aa
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 15:08:06 +0900

    116

---
+++ src/jp/ac/chiba/x15g016/kd116/X116.java
@@ -0,0 +1,19 @@
+package jp.ac.chiba.x15g016.kd116;
+
+public class X116 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int i;
+        for(i=0;i<2;i=i+1)
+           System.out.println("あいうえお　あいうえお　あいうえお\nかきくけこ　かきくけこ　かきくけこ\nさしすせそ　さしすせそ　さしすせそ");
+        /*int x;
+        for(x=0;x<2;x=x+1)	
+           System.out.println("かきくけこ　かきくけこ　かきくけこ");
+        int y;
+        for(y=0;y<2;y=y+1)	
+           System.out.println("さしすせそ　さしすせそ　さしすせそ");*/
+        
+	}
+
+}
