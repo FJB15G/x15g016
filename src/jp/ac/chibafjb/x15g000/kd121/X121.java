e9804ed3094d54f6433728266c0622eea506001e
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 15:08:11 +0900

    121

---
+++ src/jp/ac/chibafjb/x15g000/kd121/X121.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g000.kd121;
+
+public class X121 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int i;
+        for(i=1;i<11;i=i+1)
+        System.out.println(i);
+	}
+
+}
