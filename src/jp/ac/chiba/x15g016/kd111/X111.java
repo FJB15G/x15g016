2ae6ae51519e533dee874efa74bf0e53c8df74a3
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 14:05:55 +0900

    課題111

---
+++ src/jp/ac/chiba/x15g016/kd111/X111.java
@@ -0,0 +1,14 @@
+package jp.ac.chiba.x15g016.kd111;
+
+public class X111 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int i;
+        for(i=0;i<10;i=i+1)
+        	System.out.println("幸田拓馬");
+		
+		
+	}
+
+}
