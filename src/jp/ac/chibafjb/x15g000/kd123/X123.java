eca13536af7ef30c8475287450bb1e938f61d356
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 15:08:22 +0900

    123

---
+++ src/jp/ac/chibafjb/x15g000/kd123/X123.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g000.kd123;
+
+public class X123 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+        for(i=1;i<11;i=i+1)
+        	System.out.print("☆");
+	}
+
+}
