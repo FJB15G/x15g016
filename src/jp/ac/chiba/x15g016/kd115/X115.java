9082d9bb239cc0692628c3ea429eda007738a2aa
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 15:08:01 +0900

    115

---
+++ src/jp/ac/chiba/x15g016/kd115/X115.java
@@ -0,0 +1,18 @@
+package jp.ac.chiba.x15g016.kd115;
+
+public class X115 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int i;
+        for(i=0;i<2;i=i+1)
+        	System.out.println("あいうえお　あいうえお　あいうえお");
+        int x;
+        for(x=0;x<2;x=x+1)
+        	System.out.println("かきくけこ　かきくけこ　かきくけこ");
+         int y;
+        for(y=0;y<2;y=y+1)
+        	System.out.println("さしすせそ　さしすせそ　さしすせそ");
+	}
+
+}
