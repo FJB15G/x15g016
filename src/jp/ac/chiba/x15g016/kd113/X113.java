a699cd4ecc52cd7356708e0bd84e7816844d0d2a
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-09 14:10:03 +0900

    113

---
+++ src/jp/ac/chiba/x15g016/kd113/X113.java
@@ -0,0 +1,13 @@
+package jp.ac.chiba.x15g016.kd113;
+
+public class X113 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int i;
+        for(i=0;i<3;i=i+1)
+        	System.out.println("*");
+        
+	}
+
+}
