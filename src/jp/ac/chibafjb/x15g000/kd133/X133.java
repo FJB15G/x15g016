89c607581a7a77ce59286b577ec5015897bb5dff
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:05:33 +0900

    133

---
+++ src/jp/ac/chibafjb/x15g000/kd133/X133.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g000.kd133;
+
+public class X133 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x=10;
+		int y=5;
+		int z=x+y;
+		System.out.print("x+y="+z);
+
+	}
+
+}
