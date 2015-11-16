5094fbd44368eb5f28d0a5f1319319acf5b0b590
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:05:54 +0900

    136

---
+++ src/jp/ac/chibafjb/x15g000/kd136/X136.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g000.kd136;
+
+public class X136 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a=100;
+		int b=1;
+		int c=1;
+		c=a;
+		a=b;
+		b=c;
+		c=a;
+		System.out.print("a="+a+"b="+b+"c="+c);
+
+	}
+
+}
