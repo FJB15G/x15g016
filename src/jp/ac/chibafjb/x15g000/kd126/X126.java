af16b465209df76246be33dfd7efa3eeba916468
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:05:12 +0900

    126

---
+++ src/jp/ac/chibafjb/x15g000/kd126/X126.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g000.kd126;
+
+public class X126 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<3;i=i+1)
+			System.out.println("12345");
+
+	}
+
+}
