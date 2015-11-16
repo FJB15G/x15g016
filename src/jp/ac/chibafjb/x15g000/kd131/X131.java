8d1da9b255bfc0d7a5347326b85e83bd0832cde3
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:05:19 +0900

    131

---
+++ src/jp/ac/chibafjb/x15g000/kd131/X131.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g000.kd131;
+
+public class X131 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int x=10;
+        System.out.print(x);
+	}
+
+}
