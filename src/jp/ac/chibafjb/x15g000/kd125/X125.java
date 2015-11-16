85fe34cedba9de727392f5356d2fbcd6b03795ab
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:05:04 +0900

    125

---
+++ src/jp/ac/chibafjb/x15g000/kd125/X125.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g000.kd125;
+
+public class X125 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=10;i<40;i=i+10)
+			System.out.println(i+"***");
+
+	}
+
+}
