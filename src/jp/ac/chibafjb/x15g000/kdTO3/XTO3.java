dc99789e3c00b2ce37b8c05336ce9c4f17fa3be0
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:04:50 +0900

    TO3

---
+++ src/jp/ac/chibafjb/x15g000/kdTO3/XTO3.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g000.kdTO3;
+
+public class XTO3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=55;i<75;i=i+5)
+			System.out.println(i);
+
+	}
+
+}
