2ea441512af53b255f055f83a33a5d9171c05158
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:04:43 +0900

    TO2

---
+++ src/jp/ac/chibafjb/x15g000/kdT02/XTO2.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g000.kdT02;
+
+public class XTO2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=100;i<103;i=i+1)
+			System.out.println(i);
+
+	}
+
+}
