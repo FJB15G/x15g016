e9f7f92df7387823d507a3707d8e0d2bfa41608a
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:05:48 +0900

    135

---
+++ src/jp/ac/chibafjb/x15g000/kd135/X135.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g000.kd135;
+
+public class X135 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str1;
+		str1=apple;
+		
+		
+	    
+				
+
+	}
+
+}
