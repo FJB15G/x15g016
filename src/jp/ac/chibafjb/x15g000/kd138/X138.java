daab33200a9faa6726c613281f948ac9f9e1f872
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:06:09 +0900

    138

---
+++ src/jp/ac/chibafjb/x15g000/kd138/X138.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g000.kd138;
+
+public class X138 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a=1;
+		int b=2;
+		int c=3;
+		int d=0;
+		d=a;
+		a=b;
+		b=c;
+	    c=d;
+		System.out.print("a="+a+"b="+b+"c="+c);
+
+	}
+
+}
