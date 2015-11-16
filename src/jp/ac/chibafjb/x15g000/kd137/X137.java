69d0d621ed4275a56cf0e2d6898fa24fe33ba988
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:06:02 +0900

    137

---
+++ src/jp/ac/chibafjb/x15g000/kd137/X137.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g000.kd137;
+
+public class X137 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a=100;
+		int b=1;
+		int c=1;
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.print("a="+a+"b="+b+"c="+c);
+
+	}
+
+}
