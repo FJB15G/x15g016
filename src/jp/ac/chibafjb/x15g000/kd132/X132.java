c380e852282a73710f31ebefab8499f5a876a514
unknown <x15g016@PC504N020.chiba-fjb.ac.jp>
2015-11-16 15:05:27 +0900

    132

---
+++ src/jp/ac/chibafjb/x15g000/kd132/X132.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g000.kd132;
+
+public class X132 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x=10;
+		System.out.print("x="+x);
+ 
+	}
+
+}
